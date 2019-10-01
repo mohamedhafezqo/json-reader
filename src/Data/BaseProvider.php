48c82eed91a02f78d62064df95f8ed90da2fd13f
Hafez <mohamed.hafezqo@gmail.com>
2019-10-01 20:24:21 +0200

    Added BaseProvider.php

---
+++ src/Data/BaseProvider.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Data;
+
+use JsonMachine\JsonMachine;
+
+/**
+ * Class BaseProvider
+ * @package App\Data
+ */
+abstract class BaseProvider
+{
+    /** @var array $query */
+    private $query;
+
+    /**
+     * @param \IteratorAggregate $iteratorResults
+     * @return array
+     */
+    abstract protected function map(\IteratorAggregate $iteratorResults): array;
+
+    /**
+     * @param string $filePath
+     * @return array
+     */
+    protected function getFileContent(string $filePath): array
+    {
+        $stream = $this->openFile($filePath);
+        $iteratorResults = JsonMachine::fromStream($stream, "/transactions");
+
+        return $this->map($iteratorResults);
+    }
+
+    /**
+     * @param string $filePath
+     * @return bool|resource
+     * @throws \Exception
+     */
+    protected function openFile(string $filePath)
+    {
+        $stream = fopen($filePath, 'r');
+
+        if (false === $stream) {
+            throw new \Exception('Unable to open file for read: ' . $filePath);
+        }
+
+        return $stream;
+    }
+
+    /**
+     * @param array $query
+     * @return $this
+     */
+    public function setQuery(array $query)
+    {
+        $this->query = $query;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getQuery(): array
+    {
+        return $this->query;
+    }
+}
